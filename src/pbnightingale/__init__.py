852ffe94f544795c5f72e0607434979721d3e5f5
Philippe Poilbarbe <ppoilbarbe@users.noreply.github.com>
2026-09-22 19:29:02 +0200

    Bump version to 0.3.0

--- src/pbnightingale/__init__.py
+++ src/pbnightingale/__init__.py
@@ -3,7 +3,7 @@
 import builtins
 
 #: Current release version (semver).
-__version__ = "0.2.2"
+__version__ = "0.3.0"
 #: Upstream author's name.
 __author__ = "Marcel Spock"
 #: Upstream author's contact address.
